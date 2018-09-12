bddc71b251f5e98e206ff6311832ffa8117335ac
Auston Hefling <auston.hefling@outlook.com>
2018-09-12 10:55:36 -0400

    add class with exercise methods

---
+++ PracticeExercises/BasicDeclarations_Expressions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercises
+{
+    public class BasicDeclarations_Expressions
+    {
+        public void Exercise1(string name, string DOB, string Mobile)
+        {
+            var myName   = $"Name   : {name}";
+            var myDOB    = $"DOB    : {DOB}";
+            var myMobile = $"Mobile : {Mobile}";
+
+            Console.WriteLine(myName);
+            Console.WriteLine(myDOB);
+            Console.WriteLine(myMobile);
+        }
+        public string Exercise2()
+        {
+            var reverse = "";
+
+            char char1 = 'X';
+            char char2 = 'M';
+            char char3 = 'L';
+            
+            reverse = $"The inverse of : {char1}{char2}{char3}";
+            reverse += $"\nis             : {char3}{char2}{char1}";
+            
+            return reverse;
+        }
+        public void Exercise3()
+        {
+            double answer = 0;
+            Console.WriteLine("Multiplication!");
+            Console.WriteLine("Enter your first number: ");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your third number: ");
+            double input3 = Convert.ToDouble(Console.ReadLine());
+
+            answer = input1 * input2 * input3;
+
+            Console.WriteLine($"{input1} x {input2} x {input3} = {answer}");
+        }
+
+    }
+}
--- PracticeExercises/Program.cs
+++ PracticeExercises/Program.cs
@@ -10,6 +10,18 @@ namespace PracticeExercises
     {
         static void Main(string[] args)
         {
+            BasicDeclarations_Expressions examples = new BasicDeclarations_Expressions();
+            // Prints name dob and mobile
+            //examples.Exercise1("Auston Hefling", "June 15, 1995", "(509) 675-5183");
+
+            // Simple version of printing something in reverse
+            //Console.WriteLine(examples.Exercise2());
+
+            // Simple multiplication method
+            //examples.Exercise3();
+
+            // 
+
         }
     }
 }
